8f818325e842c72b4171b3eec6d4861710fd88ba
Tanu <tanushreepardhi93@gmail.com>
2026-08-05 18:50:31 +0530

    connectivity

---
+++ data.py
@@ -0,0 +1,30 @@
+import mysql.connector as m
+
+con = m.connect(
+    host="localhost",
+    port="3306",
+    user="root",
+    password="abc123",
+    database="Freshers",
+  
+)
+
+if(con.is_connected):
+    print("Connected Successfully")
+else:
+    print("err in connection")
+
+cursor=con.cursor()
+
+
+sql=sql = "INSERT INTO unity(stu_name, stu_intrest, ph_no) VALUES(%s, %s, %s)"
+stu_name=input("Enter Name :")
+stu_intrest=input("Enter Intrest :")
+ph_no=input("Enter phone no :")
+t=(stu_name,stu_intrest,ph_no)
+
+cursor.execute(sql,t)
+
+con.commit()
+print("Data Inserted")
+
